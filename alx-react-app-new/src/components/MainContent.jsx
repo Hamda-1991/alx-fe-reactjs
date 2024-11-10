bf15748a4aab7d31ceab4e3afb9dbca6801dd2ac
Hamda Hassan <hassanodowa7@gmail.com>
2024-11-10 23:05:38 +0300

    add

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,13 +3,14 @@ function MainContent() {
   return (
     <div
       style={{
-        backgroundColor: "lightblue",
         padding: "20px",
+        backgroundColor: "lightblue",
         borderRadius: "8px",
       }}
     >
       <h1>Welcome to Main Content</h1>
       <p>This is the main content area of the application.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </div>
   );
 }
