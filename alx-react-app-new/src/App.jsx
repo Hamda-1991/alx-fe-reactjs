437aa173ac66d4ccfb4c6bc3b8b3396671ee65c4
Hamda Hassan <hassanodowa7@gmail.com>
2024-11-10 23:16:38 +0300

    add

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
       <h1>User Profiles</h1>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserProfile name="Bob" age="30" bio="An avid traveler and foodie." />
+      <div>
+        <h1>Counter Application</h1>
+        <Counter /> {/* Use Counter component */}
+      </div>
     </div>
   );
 }
