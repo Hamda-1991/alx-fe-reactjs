fb0bb8c0624b508e98d9094551cc231c957db685
Hamda Hassan <hassanodowa7@gmail.com>
2024-11-17 20:31:04 +0300

    add'

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -2,18 +2,18 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) =>
-    set((state) => ({ recipes: [...state.recipes, recipe] })),
+  setRecipes: (newRecipes) => set(() => ({ recipes: newRecipes })), // Replace entire recipe list
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })), // Add new recipe
   updateRecipe: (id, updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
       ),
-    })),
+    })), // Update recipe by ID
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+    })), // Delete recipe by ID
 }));
 
 export default useRecipeStore;
