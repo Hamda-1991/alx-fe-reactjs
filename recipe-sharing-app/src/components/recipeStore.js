16ebc3c7dba9050afa9709e76205c18b9d228574
Hamda Hassan <hassanodowa7@gmail.com>
2024-11-17 21:20:53 +0300

    add

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,10 +1,19 @@
-// recipeStore.js
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) =>
-    set((state) => ({ recipes: [...state.recipes, recipe] })),
+  setRecipes: (newRecipes) => set(() => ({ recipes: newRecipes })), // Replace entire recipe list
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })), // Add new recipe
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })), // Update recipe by ID
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })), // Delete recipe by ID
 }));
 
-export default useRecipeStore; // Ensure this line is present for default export
+export default useRecipeStore;
--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,19 +1,22 @@
-import { create } from "zustand";
+// src/components/recipeStore.js
+import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  setRecipes: (newRecipes) => set(() => ({ recipes: newRecipes })), // Replace entire recipe list
-  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })), // Add new recipe
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
   updateRecipe: (id, updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
       ),
-    })), // Update recipe by ID
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })), // Delete recipe by ID
+    })),
 }));
 
 export default useRecipeStore;
