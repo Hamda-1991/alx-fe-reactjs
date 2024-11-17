f2b2038548d75e7a5e59ff6d121be8816afaa162
Hamda Hassan <hassanodowa7@gmail.com>
2024-11-17 23:43:51 +0300

    add

---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,39 @@
+// src/Navbar.jsx
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", backgroundColor: "#333", color: "white" }}>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li style={{ display: "inline", margin: "0 10px" }}>
+          <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+            Home
+          </Link>
+        </li>
+        <li style={{ display: "inline", margin: "0 10px" }}>
+          <Link to="/about" style={{ color: "white", textDecoration: "none" }}>
+            About
+          </Link>
+        </li>
+        <li style={{ display: "inline", margin: "0 10px" }}>
+          <Link
+            to="/services"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            Services
+          </Link>
+        </li>
+        <li style={{ display: "inline", margin: "0 10px" }}>
+          <Link
+            to="/contact"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
