349de33e2bb88fbc46dabea6ef4a53115d5ca8c2
Hamda Hassan <hassanodowa7@gmail.com>
2024-12-01 21:11:28 +0300

    add

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // You can enable darkMode if needed
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
