c697f68b42cdbc72ceb472522bd69986c4d4c073
Hamda Hassan <hassanodowa7@gmail.com>
2024-12-01 21:48:52 +0300

    add

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,25 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div
+      className="user-profile bg-gray-100 mx-auto my-20 rounded-lg shadow-lg 
+        user-profile-sm sm:user-profile-md md:user-profile-lg"
+    >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto"
+        className="rounded-full mx-auto 
+            user-profile-img-sm sm:user-profile-img-md md:user-profile-img-lg"
       />
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">
+      <h1
+        className="my-4 text-blue-800 
+          user-profile-heading-sm sm:user-profile-heading-md md:user-profile-heading-lg"
+      >
         John Doe
       </h1>
-      <p className="text-sm sm:text-base text-gray-600">
+      <p
+        className="text-gray-600 
+          user-profile-paragraph-sm sm:user-profile-paragraph-md"
+      >
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
