1b52250198807de2db3d92c2c6bb9f43e48bf90a
Hamda Hassan <hassanodowa7@gmail.com>
2024-12-08 20:20:37 +0300

    Create Home Page for Recipe Sharing Platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="text-blue-500 text-center">
       <h1>Welcome to the Recipe Sharing Platform</h1>
+      <HomePage />;
     </div>
   );
 }
