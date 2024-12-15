45ba6a506fc157868d8db0f195f60877bcd9a8e5
Hamda Hassan <hassanodowa7@gmail.com>
2024-12-15 23:15:31 +0300

    add

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,22 +1,26 @@
-// src/services/githubService.js
-import axios from "axios";
+import axios from 'axios';
 
-const GITHUB_API_URL = "https://api.github.com/search/users";
+const BASE_URL = 'https://api.github.com';
 
-// Fetch user data from GitHub API with advanced search parameters
-export const fetchUserData = async (query, location = "", minRepos = "") => {
-  let searchQuery = `${query}`;
-
-  // Add location and minRepos to the query if provided
-  if (location) searchQuery += `+location:${location}`;
-  if (minRepos) searchQuery += `+repos:>=${minRepos}`;
-
+export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(`${GITHUB_API_URL}?q=${searchQuery}`);
-    return response.data.items; // Return the list of users found
+    const response = await axios.get(`${BASE_URL}/users/${username}`);
+    return response.data;
   } catch {
     throw new Error("User not found");
   }
 };
+
+export const fetchAdvancedSearchResults = async (query) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/search/users`, {
+      params: { q: query },
+    });
+    return response.data.items; // The `items` array contains the list of users
+  } catch {
+    throw new Error("User not found");
+  }
+};
+
 
 
